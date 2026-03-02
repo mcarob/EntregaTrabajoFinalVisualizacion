750139eeec506d9c6963b25dc8bf54ea920bc1f0
mcarob <37860253+mcarob@users.noreply.github.com>
2026-03-01 22:09:03 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // If you deploy to GitHub Pages under a repo subpath, set base to "/<repo-name>/"
 export default defineConfig({
   plugins: [react()],
+  base: "/EntregaTrabajoFinalVisualizacion/",
 });
